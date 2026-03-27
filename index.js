3a57ee37c8e94b143a88a68c8ca2fe35c7ce51be
Agbonyin Clement <agbonyinclement@gmail.com>
2026-03-26 23:03:55 -0500

    changes in last line

--- index.js
+++ index.js
@@ -43,26 +43,6 @@ const students = [
 ];
 
 
-let user= [{
-    "userId": 1,
-    
-    "id": 1,
-    "title": "Ligend of the Giver",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "We aren't losers",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-  },
-  {
-    "userId": 1,
-    "id": 3,
-    "title": "Best Software",
-    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-  }];
-
   
 
 app.get('/', (req, res) => {
@@ -74,7 +54,7 @@ app.post('/deleteUser/:id', (req, res) => {
 
     students.splice(id, 1);
 
-    // res.redirect('/del'); // ✅ no data here
+    res.redirect('/del');
 });
 
 app.get('/del', (req, res) => {
@@ -90,3 +70,4 @@ if (err) {
    console.log("Server is running on http://localhost:5009");
 }
 });
+
